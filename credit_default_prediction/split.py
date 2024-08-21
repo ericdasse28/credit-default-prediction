07f9740790614c214b8825767b7aeeda8ed7c370
Eric Dasse <christ.gououi@gmail.com>
2024-08-21 20:50:18 +0200

    Add split script

---
+++ credit_default_prediction/split.py
@@ -0,0 +1,44 @@
+import argparse
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def get_features_and_labels(loan_data: pd.DataFrame) -> tuple[
+    pd.Series,
+    pd.Series,
+]:
+    X = loan_data.drop("loan_status", axis=1)
+    y = loan_data["loan_status"]
+
+    return X, y
+
+
+def split_data(loan_data):
+    X, y = get_features_and_labels(loan_data)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=123
+    )
+    return X_train, X_test, y_train, y_test
+
+
+def save_loan_data(X, y, save_path):
+    loan_data = pd.concat([X, y], axis=1)
+    loan_data.to_csv(save_path)
+
+
+def _get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--preprocessed-data-path")
+    parser.add_argument("--train-path")
+    parser.add_argument("--test-path")
+
+
+def main():
+    args = _get_arguments()
+
+    loan_data = pd.read_csv(args.preprocessed_data_path)
+    X_train, X_test, y_train, y_test = split_data(loan_data)
+
+    save_loan_data(X_train, y_train, args.train_path)
+    save_loan_data(X_test, y_test, args.test_path)
