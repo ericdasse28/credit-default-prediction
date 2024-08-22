969f1cdc8b769e01b79717af2b53bf5ff62287ce
Eric Dasse <christ.gououi@gmail.com>
2024-08-22 11:38:56 +0200

    Add evaluation script

--- credit_default_prediction/dataset.py
+++ credit_default_prediction/dataset.py
@@ -1,5 +1,7 @@
 """Dataset-related functions."""
 
+import os
+
 import pandas as pd
 
 
@@ -9,5 +11,15 @@ def get_features_and_labels(loan_data: pd.DataFrame) -> tuple[
 ]:
     X = loan_data.drop("loan_status", axis=1)
     y = loan_data["loan_status"]
+
+    return X, y
+
+
+def read_features_and_labels(dataset_path: os.PathLike) -> tuple[
+    pd.Series,
+    pd.Series,
+]:
+    loan_data = pd.read_csv(dataset_path)
+    X, y = get_features_and_labels(loan_data)
 
     return X, y
---
+++ credit_default_prediction/evaluate.py
@@ -0,0 +1,45 @@
+"""Evaluation script."""
+
+import argparse
+
+import joblib
+import pandas as pd
+from dvclive import Live
+from sklearn.base import accuracy_score
+from sklearn.metrics import precision_score, recall_score
+
+from credit_default_prediction.dataset import read_features_and_labels
+
+
+def evaluate(model, X: pd.Series, y: pd.Series) -> dict:
+    y_pred = model.predict(X)
+
+    return {
+        "accuracy": accuracy_score(y, y_pred),
+        "precision": precision_score(y, y_pred),
+        "recall": recall_score(y, y_pred),
+    }
+
+
+def save_model_metrics(metrics: dict):
+    with Live() as live:
+        for metric in metrics.keys():
+            live.log_metric(f"test/{metric}", metrics[metric])
+
+
+def _get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-path")
+    parser.add_argument("--test-dataset-path")
+
+    return parser.parse_args()
+
+
+def main():
+
+    args = _get_arguments()
+    model = joblib.load(args.model_path)
+    X_test, y_test = read_features_and_labels(args.test_dataset_path)
+
+    metrics = evaluate(model, X_test.values, y_test.values)
+    save_model_metrics(metrics)
