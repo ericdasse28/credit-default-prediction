e325472c5754eeac478ef9cebb20ebf8b9838604
Eric Dasse <christ.gououi@gmail.com>
2024-08-28 18:28:35 +0200

    Increase threshold to 0.3

--- credit_default_prediction/evaluate.py
+++ credit_default_prediction/evaluate.py
@@ -15,7 +15,7 @@ from sklearn.metrics import (  # noqa
 from credit_default_prediction.dataset import read_features_and_labels
 from dvclive import Live
 
-THRESHOLD = 0.25
+THRESHOLD = 0.3
 
 
 def evaluate(model, X: pd.Series, y: pd.Series) -> dict:
