e7d5f2cdb875af54d5dec2d0a4251d4e4d05bc80
Eric Dasse <christ.gououi@gmail.com>
2024-08-30 16:11:57 +0200

    fix: use the right yaml scope to get params

--- credit_default_prediction/params.py
+++ credit_default_prediction/params.py
@@ -5,8 +5,13 @@ import yaml
 PARAMS_FILE_PATH = Path(__file__).parent.parent / "params.yaml"
 
 
-def load_pipeline_params():
+def load_pipeline_params() -> dict:
     with open(PARAMS_FILE_PATH) as params_file:
         pipeline_params = yaml.safe_load(params_file)
 
     return pipeline_params
+
+
+def load_stage_params(stage_name: str) -> dict:
+    pipeline_params = load_pipeline_params()
+    return pipeline_params[stage_name]
--- credit_default_prediction/split.py
+++ credit_default_prediction/split.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from credit_default_prediction.dataset import get_features_and_labels
-from credit_default_prediction.params import load_pipeline_params
+from credit_default_prediction.params import load_stage_params
 
 
 @dataclass
@@ -26,7 +26,7 @@ def split_data(loan_data: pd.DataFrame, split_params: SplitParams):
 
 
 def load_split_params() -> SplitParams:
-    pipeline_params = load_pipeline_params()
+    pipeline_params = load_stage_params("split")
     return SplitParams(
         test_size=pipeline_params["test_size"],
         random_state=pipeline_params["random_state"],
