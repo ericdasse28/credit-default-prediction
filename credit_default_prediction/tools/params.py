c4c8143f8ba7cf33014c4d516234ebc8bfb7144e
Eric Dasse <christ.gououi@gmail.com>
2024-10-20 13:28:17 +0200

    Feat: `get_hyperparameters` function

--- credit_default_prediction/tools/params.py
+++ credit_default_prediction/tools/params.py
@@ -15,3 +15,14 @@ def load_pipeline_params() -> dict:
 def load_stage_params(stage_name: str) -> dict:
     pipeline_params = load_pipeline_params()
     return pipeline_params[stage_name]
+
+
+def _get_hyperparameters_from_config(params_file_path):
+    with open(params_file_path) as params_file:
+        params = yaml.safe_load(params_file)
+
+    return params["train"]
+
+
+def get_hyperparameters():
+    return _get_hyperparameters_from_config(PARAMS_FILE_PATH)
