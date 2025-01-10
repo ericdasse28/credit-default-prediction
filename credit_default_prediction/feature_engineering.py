da8d5290906f15f5cc1bd2343737f00a391943b4
Eric Dasse <christ.gououi@gmail.com>
2025-01-10 15:48:10 +0100

    Fix feature engineering test

--- credit_default_prediction/feature_engineering.py
+++ credit_default_prediction/feature_engineering.py
@@ -19,6 +19,10 @@ def engineer_features(clean_loan_data: pd.DataFrame) -> pd.DataFrame:
     feature_engineered_data = clean_loan_data[get_important_features()]
     # One-hot encoding
     feature_engineered_data = pd.get_dummies(feature_engineered_data)
+    # Log tranform large features
+    feature_engineered_data = log_transform_large_features(
+        feature_engineered_data,
+    )
 
     return feature_engineered_data
 
