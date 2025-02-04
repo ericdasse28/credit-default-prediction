65f1057e6a92a55b8ae36545defcd307365aa410
Eric Dasse <christ.gououi@gmail.com>
2025-02-04 20:02:27 +0100

    `split_data`: add type hinting

--- credit_default_prediction/split.py
+++ credit_default_prediction/split.py
@@ -14,7 +14,7 @@ class SplitParams:
     random_state: int
 
 
-def split_data(loan_data: pd.DataFrame, split_params: SplitParams):
+def split_data(loan_data: pd.DataFrame, split_params: SplitParams) -> tuple:
     X, y = get_features_and_labels(loan_data)
     X_train, X_test, y_train, y_test = train_test_split(
         X,
