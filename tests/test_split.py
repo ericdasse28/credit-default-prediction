4932677af58a5447a11a5158cc30b0342cc6999a
Eric Dasse <christ.gououi@gmail.com>
2025-02-04 20:17:31 +0100

    Feat(split_data): negative test for data splitting task

--- tests/test_split.py
+++ tests/test_split.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pytest
 
 from credit_default_prediction.split import SplitParams, split_data
 
@@ -24,3 +25,24 @@ def test_split_data():
     assert len(y_train) == 2
     assert len(X_test) == 1
     assert len(y_test) == 1
+
+
+def test_split_data_missing_loan_status():
+    """Given loan applications dataframe with a missing 'loan_status' column
+      and split parameters,
+    When we split the loan applications dataframe using said parameters,
+    Then a KeyError exception about the missing 'loan_status' column is raised."""
+
+    dummy_data = pd.DataFrame(
+        {
+            "person_age": [22, 25, 40],
+            "person_income": [35000, 45000, 52000],
+            "loan_intent": ["PERSONAL", "MEDICAL", "VENTURE"],
+        }
+    )
+    dummy_parameters = SplitParams(test_size=0.2, random_state=3)
+
+    with pytest.raises(KeyError) as e_info:
+        X_train, X_test, y_train, y_test = split_data(dummy_data, dummy_parameters)
+
+    assert "loan_status" in str(e_info.value)
