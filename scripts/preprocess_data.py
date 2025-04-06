422c6525f14c62fd7995aa059234e42cbfc68244
Eric Dasse <christ.gououi@gmail.com>
2025-04-06 19:38:07 +0200

    Feat: create script directory and add preprocessing script

---
+++ scripts/preprocess_data.py
@@ -0,0 +1,21 @@
+import argparse
+
+import pandas as pd
+
+from credit_default_prediction.preprocess_data import preprocess_data
+
+
+def _get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--raw-data-path")
+    parser.add_argument("--preprocessed-data-path")
+
+    return parser.parse_args()
+
+
+def main():
+    args = _get_arguments()
+
+    loan_data = pd.read_csv(args.raw_data_path)
+    loan_data = preprocess_data(loan_data)
+    loan_data.to_csv(args.preprocessed_data_path, index=False)
