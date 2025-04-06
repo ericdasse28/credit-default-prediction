38d84481a9953b4836e0449ca687ce2df925458b
Eric Dasse <christ.gououi@gmail.com>
2025-04-06 21:12:38 +0200

    Introduce HyperParams dataclass

--- credit_default_prediction/training.py
+++ credit_default_prediction/training.py
@@ -1,12 +1,22 @@
 """Model training script."""
 
+from dataclasses import asdict, dataclass
+
 import joblib
 import pandas as pd
 import xgboost as xgb
 
 
-def train(X: pd.Series, y: pd.Series, hyper_parameters: dict):
-    loan_default_classifier = xgb.XGBClassifier(**hyper_parameters)
+@dataclass
+class HyperParams:
+    learning_rate: float
+    max_depth: 4
+    min_child_wieght: 1
+
+
+def train(X: pd.Series, y: pd.Series, hyper_parameters: HyperParams):
+    hyper_parameters_dict = asdict(hyper_parameters)
+    loan_default_classifier = xgb.XGBClassifier(**hyper_parameters_dict)
     loan_default_classifier.fit(X, y)
 
     return loan_default_classifier
