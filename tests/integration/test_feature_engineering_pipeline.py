9a4a07b70914a3bf73b81ec06406645faa7b7fb6
Eric Dasse <christ.gououi@gmail.com>
2025-09-12 18:15:43 +0200

    Fix test

--- tests/integration/test_feature_engineering_pipeline.py
+++ tests/integration/test_feature_engineering_pipeline.py
@@ -11,28 +11,30 @@ def test_engineer_features():
 
     clean_loan_data = pd.DataFrame(
         {
-            "person_age": [21, 25, 23, 24],
             "person_income": [
                 33600,
                 42000,
                 49500,
                 75000,
             ],
-            "person_home_ownership": ["OWN", "MORTGAGE", "RENT", "RENT"],
             "person_emp_length": [5.0, 1.0, 4.0, 8.0],
-            "loan_intent": ["EDUCATION", "MEDICAL", "MEDICAL", "MEDICAL"],
-            "loan_grade": ["B", "C", "C", "C"],
+            "person_age": [21, 25, 23, 24],
+            "loan_percent_income": [0.1, 0.57, 0.53, 0.55],
+            "loan_int_rate": [11.14, 12.87, 15.23, 14.27],
             "loan_amnt": [
                 5000,
                 6000,
                 6000,
                 9600,
             ],
-            "loan_int_rate": [11.14, 12.87, 15.23, 14.27],
             "loan_status": [0, 1, 1, 1],
-            "loan_percent_income": [0.1, 0.57, 0.53, 0.55],
-            "cb_person_default_on_file": [0, 0, 0, 1],
-            "cb_person_cred_hist_length": [2, 3, 2, 4],
+            "loan_grade_B": [True, False, False, False],
+            "loan_grade_C": [False, True, True, True],
+            "loan_intent_EDUCATION": [True, False, False, False],
+            "loan_intent_MEDICAL": [False, True, True, True],
+            "person_home_ownership_MORTGAGE": [False, True, False, False],
+            "person_home_ownership_OWN": [True, False, False, False],
+            "person_home_ownership_RENT": [False, False, True, True],
         }
     )
     feature_engineer = FeatureEngineer()
