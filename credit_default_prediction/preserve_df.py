f472c17b718ad45edd9121fec5fb61aff011695a
Eric Dasse <christ.gououi@gmail.com>
2025-09-12 18:02:21 +0200

    Feat(PreserveDF): Handle transformers that don't have `get_feature_names_out` attribute safely

--- credit_default_prediction/preserve_df.py
+++ credit_default_prediction/preserve_df.py
@@ -3,15 +3,20 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class PreserveDF(BaseEstimator, TransformerMixin):
-    def __init__(self, transformer):
+    def __init__(self, transformer: TransformerMixin):
         self.transformer = transformer
-        self.columns = None
+        self.columns_ = None
 
     def fit(self, X: pd.DataFrame, y=None):
-        self.columns = X.columns
         self.transformer.fit(X, y)
+        # Try to get output feature names safely
+        try:
+            self.columns_ = self.transformer.get_feature_names_out()
+        except (AttributeError, NotImplementedError):
+            # Fall back to input columns if not available
+            self.columns_ = getattr(X, "columns", None)
         return self
 
     def transform(self, X: pd.DataFrame, y=None):
         X_t = self.transformer.transform(X)
-        return pd.DataFrame(X_t, columns=self.columns, index=X.index)
+        return pd.DataFrame(X_t, columns=self.columns_, index=X.index)
