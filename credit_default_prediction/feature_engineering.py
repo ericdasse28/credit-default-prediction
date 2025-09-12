de262fc8ef4928eb68e4e5daf4bf645dcf5a6eef
Eric Dasse <christ.gououi@gmail.com>
2025-09-12 08:25:45 +0200

    Feat: add feature engineer

--- credit_default_prediction/feature_engineering.py
+++ credit_default_prediction/feature_engineering.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
 
 from credit_default_prediction.tools import params
 
@@ -35,3 +36,11 @@ def engineer_features(clean_loan_data: pd.DataFrame) -> pd.DataFrame:
     )
 
     return feature_engineered_data
+
+
+class FeatureEngineer(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X: pd.DataFrame):
+        return engineer_features(X)
