7b63bf2156335f3b600966be5a80d823e40d8538
Eric Dasse <christ.gououi@gmail.com>
2025-09-13 13:41:28 +0200

    Refactor: move function

--- credit_default_prediction/tools/params.py
+++ credit_default_prediction/tools/params.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import yaml
 
-from credit_default_prediction.tools import params
+from credit_default_prediction.split import SplitParams
 
 PARAMS_FILE_PATH = Path(__file__).parent.parent.parent / "params.yaml"
 
@@ -14,7 +14,7 @@ def load_pipeline_params() -> dict:
     return pipeline_params
 
 
-def load_stage_params(stage_name: str) -> dict:
+def _load_stage_params(stage_name: str) -> dict:
     pipeline_params = load_pipeline_params()
     return pipeline_params[stage_name]
 
@@ -31,5 +31,13 @@ def get_hyperparameters():
 
 
 def get_important_features() -> list[str]:
-    preprocess_params = params.load_stage_params("feature_engineering")
+    preprocess_params = _load_stage_params("feature_engineering")
     return preprocess_params["important_columns"]
+
+
+def load_split_params() -> SplitParams:
+    pipeline_params = _load_stage_params("split")
+    return SplitParams(
+        test_size=pipeline_params["test_size"],
+        random_state=pipeline_params["random_state"],
+    )
--- scripts/split.py
+++ scripts/split.py
@@ -1,8 +1,8 @@
 import argparse
 from pathlib import Path
 
-from credit_default_prediction.split import SplitParams, split_data_from_path
-from credit_default_prediction.tools.params import load_stage_params
+from credit_default_prediction.split import split_data_from_path
+from credit_default_prediction.tools import params
 
 
 def _get_arguments():
@@ -14,19 +14,11 @@ def _get_arguments():
     return parser.parse_args()
 
 
-def _load_split_params() -> SplitParams:
-    pipeline_params = load_stage_params("split")
-    return SplitParams(
-        test_size=pipeline_params["test_size"],
-        random_state=pipeline_params["random_state"],
-    )
-
-
 def main():
     args = _get_arguments()
-
-    split_params = _load_split_params()
     split_data_dir = Path(args.train_path).parent
+    split_params = params.load_split_params()
+
     split_data_from_path(
         raw_data_path=args.raw_data_path,
         split_data_dir=split_data_dir,
