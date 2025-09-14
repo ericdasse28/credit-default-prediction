18278565c5f818bfb7a982c58fedd195dedeb468
Eric Dasse <christ.gououi@gmail.com>
2025-09-14 18:52:24 +0200

    Feat: replace argparse feature engineering command

--- credit_default_prediction/feature_engineering.py
+++ credit_default_prediction/feature_engineering.py
@@ -1,3 +1,6 @@
+import os
+
+import click
 import numpy as np
 import pandas as pd
 
@@ -24,3 +27,21 @@ def engineer_features(clean_loan_data: pd.DataFrame) -> pd.DataFrame:
     )
 
     return feature_engineered_data
+
+
+@click.command(
+    help="Performs feature engineering on loan applications data that have preprocessed beforehand. This operation transforms the data to help the model learn better."
+)
+@click.option(
+    "--preprocessed-data-path",
+    help="Path to the preprocessed loan applications CSV file.",
+)
+@click.option(
+    "--feature-store-path",
+    help="Path where the feature engineered loan applications CSV file will be saved.",
+)
+def cli(preprocessed_data_path: os.PathLike, feature_store_path: os.PathLike):
+    preprocessed_data = pd.read_csv(preprocessed_data_path)
+    preprocessed_data = engineer_features(preprocessed_data)
+    # Save feature engineered loan applications
+    preprocessed_data.to_csv(feature_store_path, index=False)
