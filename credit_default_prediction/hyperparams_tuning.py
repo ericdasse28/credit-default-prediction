63d8b20b30b6171db444a560d48fb1701363f656
Eric Dasse <christ.gououi@gmail.com>
2025-09-14 22:15:05 +0200

    Refactor hyperparameter tuning

--- credit_default_prediction/hyperparams_tuning.py
+++ credit_default_prediction/hyperparams_tuning.py
@@ -1,7 +1,8 @@
 """Hyper-parameter tuning script."""
 
-import argparse
+import os
 
+import click
 import numpy as np
 import xgboost as xgb
 from sklearn.model_selection import GridSearchCV
@@ -9,18 +10,18 @@ from sklearn.model_selection import GridSearchCV
 from credit_default_prediction.dataset import LoanApplications
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset-path")
-    args = parser.parse_args()
-    train_dataset = LoanApplications.from_path(args.dataset_path)
+@click.command(
+    help="Hyper-parameters tuning using training dataset. Prints the best parameters on the standard output."
+)
+@click.option("--train-data-path", help="Path to training dataset.")
+def cli(train_data_path: os.PathLike):
+    train_dataset = LoanApplications.from_path(train_data_path)
 
     params_to_test = {
         "learning_rate": np.arange(0, 1, 0.1),
         "max_depth": range(3, 10),
         "min_child_weight": range(1, 6),
     }
-
     gsearch = GridSearchCV(
         estimator=xgb.XGBClassifier(
             learning_rate=0.3,
@@ -30,4 +31,5 @@ def main():
         scoring="roc_auc",
     )
     gsearch.fit(train_dataset.X, train_dataset.y)
+
     print(gsearch.best_params_, gsearch.best_score_)
