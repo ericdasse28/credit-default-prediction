3d67c64639978a51d49ffbf3ddefb29b7097d1e4
Eric Dasse <christ.gououi@gmail.com>
2026-04-07 00:01:25 +0200

    Feat: add DVC experiment tracker

---
+++ experiment_tracking/dvc_experiment_tracker.py
@@ -0,0 +1,15 @@
+from dvclive.live import Live
+
+from experiment_tracking.experiment_tracker import ExperimentTracker
+
+
+class DVCExperimentTracker(ExperimentTracker):
+    def __init__(self, live: Live) -> None:
+        self._live = live
+
+    def log_params(self, **hyperparameters):
+        self._live.log_params(**hyperparameters)
+
+    def log_metrics(self, **metrics):
+        for metric, value in metrics.items():
+            self._live.log_metric(metric, value)
