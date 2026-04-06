ea5681a88fe2c133d77df0d9463f28b011200f33
Eric Dasse <christ.gououi@gmail.com>
2026-04-07 00:11:22 +0200

    Fix: use HyperParams class

--- credit_default_prediction/experiment_tracking/experiment_tracker.py
+++ credit_default_prediction/experiment_tracking/experiment_tracker.py
@@ -1,6 +1,8 @@
 from typing import Protocol
 
+from credit_default_prediction.training import HyperParams
+
 
 class ExperimentTracker(Protocol):
-    def log_params(self, **hyperparameters): ...
+    def log_params(self, hyperparameters: HyperParams): ...
     def log_metrics(self, **metrics): ...
