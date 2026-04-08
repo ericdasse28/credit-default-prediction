b2a4ef96984053bdc383c0f81b7df7d8bd608814
Eric Dasse <christ.gououi@gmail.com>
2026-04-08 08:03:13 +0200

    Feat: add `phase` to `log_metrics` method signature on ExperimentTracker protocol

--- credit_default_prediction/experiment_tracking/experiment_tracker.py
+++ credit_default_prediction/experiment_tracking/experiment_tracker.py
@@ -1,8 +1,13 @@
-from typing import Protocol
+from typing import Literal, Protocol
 
 from credit_default_prediction.hyper_params import HyperParams
 
 
 class ExperimentTracker(Protocol):
     def log_params(self, hyperparameters: HyperParams): ...
-    def log_metrics(self, metrics: dict[str, float]): ...
+
+    def log_metrics(
+        self,
+        metrics: dict[str, float],
+        phase: Literal["cross_validation", "test"],
+    ): ...
